c5d77c65c6158fad1bab0e1cc5689e2739b85a3b
Areg <areg06.06@mail.ru>
2026-04-04 00:25:33 +0300

    Add to food creation animations

--- Assets/BugColony/Scripts/Foods/FoodView.cs
+++ Assets/BugColony/Scripts/Foods/FoodView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -18,6 +19,8 @@ namespace BugColony.Scripts.Foods
             _isActive = true;
             _pool = pool;
             _nutritionalValue = nutritionalValue;
+            transform.localScale = Vector3.zero;
+            transform.DOScale(1f, 1f).SetEase(Ease.OutBounce);
         }
 
         public int Eat()
@@ -33,12 +36,14 @@ namespace BugColony.Scripts.Foods
         private void OnDestroy()
         {
             _isActive = false;
+            transform.DOKill(true);
         }
         
         private void ReturnToPool()
         {
             _isActive = false;
             _pool?.Release(this);
+            transform.DOKill(true);
         } 
     }
 }
